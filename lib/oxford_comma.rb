f7f9865818514dc89cfaf2c100f77e9182b5cb0e
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-19 23:52:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,3 @@
 def oxford_comma(array)
-  return array.to_s if array.nil? or array.length <= 1
-  array[0..-2].join(", ") + " and " + array[-1]
+  return array[0..-2].join(", ") + " and " + array[-1]
 end
