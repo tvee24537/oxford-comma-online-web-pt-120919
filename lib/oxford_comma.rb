c0cc7c9b841efa31161450ee1f7ca45fa3a8e849
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-20 00:04:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
 def oxford_comma(array)
-
-  return array[0..-2].join(", ") + " and " + array[-1]
+  return array[0] if array.length == 1
+  return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
 end
